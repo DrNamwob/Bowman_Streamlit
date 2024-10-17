29314196e77a1b82d0fdc67d183bb915d2691c85
DrNamwob <136391240+DrNamwob@users.noreply.github.com>
2024-10-16 20:49:19 -0400

    added authkey?

--- auth.py
+++ auth.py
@@ -1,14 +1,18 @@
+import os
 import streamlit as st
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def login():
-    # Hardcoded users for demonstration; replace with your user management system
-    usernames = {"Derek": "coding_is_cool", "Kelsey": "hasaniceass"}
+    # Use AUTH_KEY from environment variables
+    auth_key = os.getenv('AUTH_KEY')
+    usernames = {"Derek": auth_key, 'CMSE830': "GoGreen1"}
 
     # Login form
     st.title("Login")
     username = st.text_input("Username")
     password = st.text_input("Password", type='password')
-
     if st.button("Login"):
         if username in usernames and usernames[username] == password:
             st.success("Login successful!")
